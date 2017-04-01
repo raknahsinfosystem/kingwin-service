42a46783fcf8d9b338273773f9f883f01eca68f8
shankar <shankar@ShankarRK>
2017-04-01 23:36:26 +0530

    first commit

---
+++ kingwin-service/src/main/java/com/raknahsinfosystem/webzion/KingwinServiceApplication.java
@@ -0,0 +1,12 @@
+package com.raknahsinfosystem.webzion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KingwinServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KingwinServiceApplication.class, args);
+	}
+}
